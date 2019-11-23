f36d51145c205a82550df5154a2d575c393879c7
Bardi Harborow <bardi@bardiharborow.com>
2019-11-24 01:51:25 +1100

    Add unit tests.

---
+++ tests/test_api.py
@@ -0,0 +1,127 @@
+# Sample data is Copyright (c) 1990-2019 IMDb.com, Inc.
+
+import requests
+import unittest
+import unittest.mock
+
+from imdb.api import IMDb, IMDbFilmographyParser
+from imdb.models import Actor, Role
+
+class TestIMDbFilmographyParser(unittest.TestCase):
+    def setUp(self):
+        self.parser = IMDbFilmographyParser()
+        self.simple_sample = """
+            <div class="filmo-row odd" id="actor-tt0119116">
+            <span class="year_column">
+            &nbsp;1997
+            </span>
+            <b><a href="/title/tt0119116/?ref_=nm_flmg_act_89"
+            >The Fifth Element</a></b>
+            <br/>
+            Korben Dallas
+            </div>
+        """
+
+        self.complex_sample = """
+            <div class="filmo-row odd" id="actress-tt8771910">
+            <span class="year_column">
+            &nbsp;2020
+            </span>
+            <b><a href="/title/tt8771910/?ref_=nm_flmg_act_3"
+            >Madam C.J. Walker</a></b>
+            (TV Series) (<a href="https://pro.imdb.com/title/tt8771910?rf=cons_nm_filmo&ref_=cons_nm_filmo"
+            class="in_production">filming</a>)
+            <br/>
+            Sarah Breedlove
+            <div class="filmo-episodes">
+            - <a href="/title/tt8772006/?ref_=nm_flmg_act_3"
+            >Episode #1.1</a>
+            (2020)
+            ... Sarah Breedlove
+            </div>
+            <div class="filmo-episodes">
+            - <a href="/title/tt8782526/?ref_=nm_flmg_act_3"
+            >Episode #1.8</a>
+            ... Sarah Breedlove
+            </div>
+            <div class="filmo-episodes">
+            - <a href="/title/tt8782512/?ref_=nm_flmg_act_3"
+            >Episode #1.7</a>
+            ... Sarah Breedlove
+            </div>
+            <div class="filmo-episodes">
+            - <a href="/title/tt8782506/?ref_=nm_flmg_act_3"
+            >Episode #1.6</a>
+            ... Sarah Breedlove
+            </div>
+            <div class="filmo-episodes">
+            - <a href="/title/tt8782498/?ref_=nm_flmg_act_3"
+            >Episode #1.5</a>
+            ... Sarah Breedlove
+            </div>
+            <div id="more-episodes-tt8771910-actress" style="display:none;">
+            <img src="https://m.media-amazon.com/..." />
+            </div>
+            <div class="filmo-episodes">
+            <a href="#"
+            data-n="8"
+            onclick="...">Show all 8 episodes</a>
+            </div>
+            </div>
+        """
+
+    def test_simple(self):
+        self.parser.feed(self.simple_sample)
+        self.assertEqual(self.parser.filmography[0], Role("The Fifth Element", "1997", "Korben Dallas"))
+
+    def test_complex(self):
+        self.parser.feed(self.complex_sample)
+        self.assertEqual(self.parser.filmography[0], Role("Madam C.J. Walker", "2020", "Sarah Breedlove"))
+
+class TestIMDb(unittest.TestCase):
+    def setUp(self):
+        TestIMDbFilmographyParser.setUp(self)
+        self.suggestions_sample = """
+            {
+              "d": [
+                {
+                  "i": {
+                    "height": 1280,
+                    "imageUrl": "https://m.media-amazon.com/...",
+                    "width": 1038
+                  },
+                  "id": "nm0914612",
+                  "l": "Emma Watson (II)",
+                  "rank": 201,
+                  "s": "Actress, The Perks of Being a Wallflower (2012)",
+                  "v": [],
+                  "vt": 87
+                }
+              ],
+              "q": "emma watson",
+              "v": 1
+            }
+        """
+
+    def test_actors(self):
+        response = unittest.mock.Mock()
+        response.text = self.suggestions_sample
+        requests.get = unittest.mock.Mock(return_value=response)
+
+        actors = IMDb.actors("Emma Watson")
+
+        self.assertEqual(actors[0], Actor("nm0914612", "Emma Watson", 201, "Actress, The Perks of Being a Wallflower (2012)", []))
+        requests.get.assert_called_with("https://v2.sg.media-imdb.com/suggestion/names/e/emma_watson.json")
+
+    def test_actor(self):
+        response = unittest.mock.Mock()
+        response.text = self.complex_sample
+        requests.get = unittest.mock.Mock(return_value=response)
+
+        filmography = IMDb.actor("nm0818055")
+
+        self.assertEqual(filmography[0], Role("Madam C.J. Walker", "2020", "Sarah Breedlove"))
+        requests.get.assert_called_with("https://www.imdb.com/name/nm0818055/")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models.py
@@ -0,0 +1,62 @@
+# Sample data is Copyright (c) 1990-2019 IMDb.com, Inc.
+
+import unittest
+
+from imdb.models import Actor, Role
+
+class TestActor(unittest.TestCase):
+    def setUp(self):
+        self.actor = Actor("nm0850102", "Amanda Tapping", 2441,
+                           "Actress, Stargate SG-1 (1997-2007)", [Role("Sanctuary", "2008-2011", "Dr. Helen Magnus")])
+        self.actor2 = Actor("nm0850102", "Amanda Tapping", 2441,
+                           "Actress, Stargate SG-1 (1997-2007)", [Role("Sanctuary", "2008-2011", "Dr. Helen Magnus")])
+        self.actor3 = Actor("nm0850102", "Amanda Tapping", 2441,
+                           "Actress, Stargate SG-1 (1997-2007)", [])
+
+    def test___eq__(self):
+        self.assertEqual(self.actor, self.actor2)
+
+    def test___repr__(self):
+        self.assertEqual(repr(self.actor), 'Actor(identifier="nm0850102", name="Amanda Tapping", rank=2441, known_for="Actress, Stargate SG-1 (1997-2007)", filmography=[Role(work="Sanctuary", year="2008-2011", role="Dr. Helen Magnus")])')
+
+    def test___str__(self):
+        self.assertEqual(str(self.actor), "Amanda Tapping, ranked 2441, known for Actress, Stargate SG-1 (1997-2007)")
+
+    def test_json(self):
+        self.assertEqual(self.actor.json(), '{"identifier": "nm0850102", "name": "Amanda Tapping", "rank": 2441, "known_for": "Actress, Stargate SG-1 (1997-2007)", "filmography": [{"work": "Sanctuary", "year": "2008-2011", "role": "Dr. Helen Magnus"}]}')
+        self.assertEqual(self.actor3.json(), '{"identifier": "nm0850102", "name": "Amanda Tapping", "rank": 2441, "known_for": "Actress, Stargate SG-1 (1997-2007)", "filmography": []}')
+
+    def test_long(self):
+        self.assertEqual(self.actor.long(), "Name: Amanda Tapping\nRanked: 2441\nKnown for: Actress, Stargate SG-1 (1997-2007)\n\nFilmography (1 film):\n\nSanctuary (2008-2011), as Dr. Helen Magnus")
+        self.assertEqual(self.actor3.long(), "Name: Amanda Tapping\nRanked: 2441\nKnown for: Actress, Stargate SG-1 (1997-2007)\n\nFilmography (0 films):\n\n")
+
+    def test_from_imdb_suggestion(self):
+        payload = {
+            "id": "nm0850102",
+            "l": "Amanda Tapping",
+            "rank": 2441,
+            "s": "Actress, Stargate SG-1 (1997-2007)"
+        }
+
+        actor = Actor.from_imdb_suggestion(payload)
+
+        self.assertEqual(actor, Actor("nm0850102", "Amanda Tapping", 2441, "Actress, Stargate SG-1 (1997-2007)", []))
+
+class TestRole(unittest.TestCase):
+    def setUp(self):
+        self.role = Role("Madam C.J. Walker", "2020", "Sarah Breedlove")
+        self.role2 = Role("Madam C.J. Walker", "2020", "Sarah Breedlove")
+        self.role3 = Role("", "", "")
+
+    def test___eq__(self):
+        self.assertEqual(self.role, self.role2)
+
+    def test___repr__(self):
+        self.assertEqual(repr(self.role), 'Role(work="Madam C.J. Walker", year="2020", role="Sarah Breedlove")')
+
+    def test___str__(self):
+        self.assertEqual(str(self.role), "Madam C.J. Walker (2020), as Sarah Breedlove")
+        self.assertEqual(str(self.role3), " (???), as Unknown")
+
+if __name__ == "__main__":
+    unittest.main()
